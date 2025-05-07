538f108e052f480b70ee935e916488f62de56c9a
LymoneLM <chengziorange233@qq.com>
2025-05-07 17:48:43 +0800

    fix: Vehicle class AC

--- PythonTest/PythonPractice/Practice6/2.py
+++ PythonTest/PythonPractice/Practice6/2.py
@@ -1,31 +1,29 @@
 class Vehicle:
-    def __init__(self, speed, size, acceleration):
+    def __init__(self, speed, size, time, acceleration):
         print(f"初速度:{speed} 加速度:{acceleration} 体积:{size}")
         self.setSpeed(speed)
-        print(f"设置的初速度为:{self.speed}")
         self.size = size
+        self.time = time
         self.acceleration = acceleration
 
-    def move(self, time):
-        print(f"移动了：{self.speed * time}")
+    def move(self):
+        print(f"移动了：{self.speed * self.time}")
 
     def setSpeed(self, speed):
         self.speed = speed
+        print(f"设置的初速度为:{speed}")
 
-    def speedUp(self, time):
-        self.setSpeed(self.speed + self.acceleration * time)
-        print(f"加速完后速度是:{self.speed}")
+    def speedUp(self):
+        print(f"加速完后速度是:{self.speed + self.acceleration * self.time}")
 
-    def speedDown(self, time):
-        clac_speed = self.speed - (self.acceleration + self.size) * time
-        clac_speed = clac_speed if clac_speed > 0 else 0
-        print(f"减速完后速度是:{clac_speed}")
+    def speedDown(self):
+        print(f"减速完后速度是:{max(0, self.speed - self.acceleration * self.time)}")
 
 
 speed = int(input())
 size = int(input())
 time = int(input())
 acceleration = int(input())
-v = Vehicle(speed, size, acceleration)
-v.speedUp(time)
-v.speedDown(time)
+v = Vehicle(speed, size, time, acceleration)
+v.speedUp()
+v.speedDown()

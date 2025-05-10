a97ce35f02f691775607b82d55c5fad378d5b294
LymoneLM <chengziorange233@qq.com>
2025-05-10 14:46:39 +0800

    fix: 删除数据结构课堂作业3的无效重复文件

--- CppTest/DataStructureClassWork/Contest1055/E3.cpp
+++ CppTest/DataStructureClassWork/Contest1055/E3.cpp
@@ -1,100 +1,51 @@
-#include <iostream>
-#include <string>
-#include <queue>
-#include <sstream>
+#include <bits/stdc++.h>
 
-struct TreeNode {
-    int index;
-    TreeNode* left;
-    TreeNode* right;
+using namespace std;
 
-    TreeNode(int i) : index(i), left(nullptr), right(nullptr) {}
-};
+const int N = 30;
+int n;
+int l[N], r[N];
+bool hf[N];
+int maxCur = -1, nodeId;
 
-bool isCompleteBinaryTree(TreeNode* root, int nodeCount) {
-    if (root == nullptr) {
-        return true;
+void dfs(int u, int cur)
+{
+    if(cur > maxCur)
+    {
+        maxCur = cur;
+        nodeId = u;
     }
-
-    std::queue<TreeNode*> q;
-    q.push(root);
-
-    bool isNullAllowed = false;
-
-    while (!q.empty()) {
-        TreeNode* node = q.front();
-        q.pop();
-
-        if (node->left != nullptr) {
-            if (isNullAllowed) {
-                return false;
-            }
-            q.push(node->left);
-        } else {
-            isNullAllowed = true;
-        }
-
-        if (node->right != nullptr) {
-            if (isNullAllowed) {
-                return false;
-            }
-            q.push(node->right);
-        } else {
-            isNullAllowed = true;
-        }
-    }
-
-    return true;
+    if(l[u] != -1) dfs(l[u], 2 * cur);
+    if(r[u] != -1) dfs(r[u], 2 * cur + 1);
 }
 
-TreeNode* buildTree(const std::vector<std::string>& treeData) {
-    std::vector<TreeNode*> nodes(treeData.size());
-
-    for (int i = 0; i < treeData.size(); ++i) {
-        if (treeData[i] != "-") {
-            nodes[i] = new TreeNode(i);
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        string a, b;
+        cin >> a >> b;
+        if(a != "-")
+        {
+            l[i] = stoi(a);
+            hf[stoi(a)] = true;
         }
+        else l[i] = -1;
+        if(b != "-")
+        {
+            r[i] = stoi(b);
+            hf[stoi(b)] = true;
+        }
+        else r[i] = -1;
     }
+    int root = 0;
+    while(hf[root]) root ++;
 
-    for (int i = 0; i < treeData.size(); ++i) {
-        if (treeData[i] != "-") {
-            TreeNode* node = nodes[i];
-            int leftIndex, rightIndex;
-            std::istringstream iss(treeData[i]);
-            iss >> leftIndex >> rightIndex;
-
-            if (leftIndex != -1) {
-                node->left = nodes[leftIndex];
-            }
-
-            if (rightIndex != -1) {
-                node->right = nodes[rightIndex];
-            }
-        }
-    }
-
-    return nodes[0];
-}
-
-int main() {
-    int n;
-
-    while (std::cin >> n) {
-        std::vector<std::string> treeData(n);
-
-        for (int i = 0; i < n; ++i) {
-            std::cin >> treeData[i];
-        }
-
-        TreeNode* root = buildTree(treeData);
-        bool isComplete = isCompleteBinaryTree(root, n);
-
-        if (isComplete) {
-            std::cout << "YES " << n - 1 << std::endl;
-        } else {
-            std::cout << "NO 0" << std::endl;
-        }
-    }
-
-    return 0;
+    dfs(root, 1);
+    
+    if(maxCur == n)
+        cout << "YES" << ' ' << nodeId << endl;
+    else
+        cout << "NO" << ' ' << root << endl;
 }

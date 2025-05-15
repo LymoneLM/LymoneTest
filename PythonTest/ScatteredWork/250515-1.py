65d109191e762be8b4e78f3b1ddcae5968390cc4
LymoneLM <chengziorange233@qq.com>
2025-05-15 19:21:43 +0800

    feat: 一些py作业程序

---
+++ PythonTest/ScatteredWork/250515-1.py
@@ -0,0 +1,5 @@
+with open("./sy8-1a.txt", "r") as f:
+    x = f.read().split(',')
+    x = [eval(i) for i in x]
+with open("./sy8-1b.txt", "w") as f:
+    f.write(f"{max(x)}, {min(x)}, {sum(x)/len(x)}")

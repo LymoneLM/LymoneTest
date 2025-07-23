43df67c69ffd245aefc49206adc0c56dc3782ece
LymoneLM <chengziorange233@qq.com>
2025-07-23 19:42:34 +0800

    fix(HardwareTest/OPI_BIO): 修复多播放器选择错误问题

--- HardwareTest/OPI_BIO/utils/speaker.py
+++ HardwareTest/OPI_BIO/utils/speaker.py
@@ -1,11 +1,10 @@
 import pyaudio
 import wave
 import numpy as np
-from multiprocessing import Process
 import threading
 
 # 播放WAV音频文件
-def play_wav_file(wav_file, volume=1.0):
+def play_wav_file(wav_file, volume=1.0, device_index=0):
     # 打开WAV文件
     with wave.open(wav_file, 'rb') as wf:
         # 初始化PyAudio
@@ -14,7 +13,8 @@ def play_wav_file(wav_file, volume=1.0):
         stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                         channels=wf.getnchannels(),
                         rate=wf.getframerate(),
-                        output=True)
+                        output=True,
+                        output_device_index=device_index)
         # 读取数据并播放
         data = wf.readframes(1024)
         while data:
@@ -29,7 +29,7 @@ def play_wav_file(wav_file, volume=1.0):
         # 终止PyAudio
         p.terminate()
 
-def play_wav_file_async(wav_file, volume=1.0):
-    t = threading.Thread(target=play_wav_file, args=(wav_file, volume), daemon=True)
+def play_wav_file_async(wav_file, volume=1.0, device_index=0):
+    t = threading.Thread(target=play_wav_file, args=(wav_file, volume, device_index), daemon=True)
     t.start()
 
